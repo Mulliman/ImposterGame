6a316dc658e68b4a6df091acbf81446cdd33afdd
Sam Mullins <sammullins.co.uk@gmail.com>
2019-11-24 08:52:50 +0000

    Added swagger for documenting the APIs.

--- ImposterGame/ImposterGame.Website/Startup.cs
+++ ImposterGame/ImposterGame.Website/Startup.cs
@@ -10,10 +10,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace ImposterGame.Website
 {
@@ -34,11 +36,37 @@ namespace ImposterGame.Website
             services.AddSingleton(new OptionGridService(new[] { new HardcodedGridProvider() }));
             services.AddTransient<IPlayerService, InMemoryPlayerService>();
             services.AddTransient<IGameService, InMemoryGameService>();
+            services.AddTransient<IMemoryCache, MemoryCache>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Imposter API", Version = "v1" });
+            });
+
+            services.AddCors(o => o.AddPolicy("defaultcors", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                string swaggerJsonBasePath = string.IsNullOrWhiteSpace(c.RoutePrefix) ? "." : "..";
+                c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json", "Imposter API");
+            });
+
+            app.UseCors("defaultcors");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
